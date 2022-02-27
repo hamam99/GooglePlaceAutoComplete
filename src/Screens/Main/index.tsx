1dffffb09b4ae2b5b0bbb8a7ce1a14d3e0d6703c
Hamam <zero.saturnus@gmail.com>
2022-02-27 10:25:53 +0700

    set up mapview

--- src/Screens/Main/index.tsx
+++ src/Screens/Main/index.tsx
@@ -1,28 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapView, { Animated, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 const Main = () => {
+  // -7.782794234312435, 110.36702205341435
+  const TUGU_JOGJA_COORDINATE = {
+    latitude: -7.782794234312435,
+    longitude: 110.36702205341435,
+  };
+  const [location, setLocation] = useState({
+    latitude: -7.782794234312435,
+    longitude: 110.36702205341435,
+  });
+  const [marker, setMarker] = useState({
+    title: '',
+    description: '',
+    latitude: TUGU_JOGJA_COORDINATE.latitude,
+    longitude: TUGU_JOGJA_COORDINATE.longitude,
+  });
+
   return (
-    <GooglePlacesAutocomplete
-      placeholder="Search for a place"
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log('onPress', 'data', data);
-        console.log('onPress', 'details', details);
-        console.log(
-          'data',
-          'location',
-          details?.geometry.location.lat,
-          details?.geometry.location.lng,
-        );
-      }}
-      onFail={error => console.error(error)}
-      query={{
-        key: 'AIzaSyANgy-MLJM0s8I72oZYSfc1qp9kdvCeiUs',
-        language: 'en',
-      }}
-      fetchDetails={true}
-    />
+    <View style={{ flex: 1 }}>
+      <GooglePlacesAutocomplete
+        styles={{ backgroundColor: '#red' }}
+        placeholder="Search for a place"
+        onPress={(data, details = null) => {
+          setMarker({
+            title: details?.name ?? '',
+            description: details?.formatted_address ?? '',
+            latitude: details?.geometry.location.lat ?? 0,
+            longitude: details?.geometry.location.lng ?? 0,
+          });
+        }}
+        onFail={error => console.error(error)}
+        query={{
+          key: 'AIzaSyANgy-MLJM0s8I72oZYSfc1qp9kdvCeiUs',
+          language: 'en',
+        }}
+        fetchDetails={true}
+      />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: TUGU_JOGJA_COORDINATE.latitude,
+          longitude: TUGU_JOGJA_COORDINATE.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        <Marker
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
+          title={marker.title}
+          description={marker.description}
+        />
+      </MapView>
+    </View>
   );
 };
 
