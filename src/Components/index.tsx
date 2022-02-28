9930104a3c67fc4151fd12b38bc9db801fbd2164
Hamam <zero.saturnus@gmail.com>
2022-02-28 15:57:03 +0700

    removing react-native-google-places-autocomplete

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,5 +1,4 @@
 import AutoComplete from './AutoComplete';
 import MapViewRN from './MapViewRN';
-import PlaceAutoComplete from './PlaceAutoComplete';
 
-export { PlaceAutoComplete, MapViewRN, AutoComplete };
+export { MapViewRN, AutoComplete };
