a7fdbb62b9aacbe2863f847cf91f05994e8e3847
Hamam <zero.saturnus@gmail.com>
2022-02-28 12:07:20 +0700

    integrate map with redux

--- src/Components/MapViewRN/index.tsx
+++ src/Components/MapViewRN/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { useSelector } from 'react-redux';
 
 type IMarker = {
   latitude: number;
@@ -9,7 +10,13 @@ type IMarker = {
 };
 
 const MapViewRN = ({ marker }: { marker: IMarker }) => {
+  const TUGU_JOGJA_COORDINATE = {
+    latitude: -7.782794234312435,
+    longitude: 110.36702205341435,
+  };
+
   const ref = React.createRef<MapView>();
+  const { detailPlace } = useSelector(state => state.PlaceAutoCompleteReducer);
 
   return (
     <MapView
@@ -18,19 +25,27 @@ const MapViewRN = ({ marker }: { marker: IMarker }) => {
       style={{ flex: 1 }}
       onLayout={() => {
         ref?.current?.animateToRegion({
-          latitude: marker.latitude,
-          longitude: marker.longitude,
+          latitude:
+            detailPlace?.geometry?.location?.lat ??
+            TUGU_JOGJA_COORDINATE.latitude,
+          longitude:
+            detailPlace?.geometry?.location?.lng ??
+            TUGU_JOGJA_COORDINATE.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         });
       }}>
       <Marker
         coordinate={{
-          latitude: marker.latitude,
-          longitude: marker.longitude,
+          latitude:
+            detailPlace?.geometry?.location?.lat ??
+            TUGU_JOGJA_COORDINATE.latitude,
+          longitude:
+            detailPlace?.geometry?.location?.lng ??
+            TUGU_JOGJA_COORDINATE.longitude,
         }}
-        title={marker.title}
-        description={marker.description}
+        title={marker.name ?? ''}
+        description={marker.formatted_address ?? ''}
       />
     </MapView>
   );
