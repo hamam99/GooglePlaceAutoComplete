7e1526d6de30cf1d928fe8f098bc95a6f4613366
Hamam <zero.saturnus@gmail.com>
2022-03-01 06:53:38 +0700

    removing default coordinate

--- src/Components/MapViewRN/index.tsx
+++ src/Components/MapViewRN/index.tsx
@@ -3,20 +3,13 @@ import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { useSelector } from 'react-redux';
 
 const MapViewRN = () => {
-  const TUGU_JOGJA_COORDINATE = {
-    latitude: -7.782794234312435,
-    longitude: 110.36702205341435,
-  };
-
   const ref = React.createRef<MapView>();
   const { detailPlace } = useSelector(state => state.PlaceAutoCompleteReducer);
 
   useEffect(() => {
     ref?.current?.animateToRegion({
-      latitude:
-        detailPlace?.geometry?.location?.lat ?? TUGU_JOGJA_COORDINATE.latitude,
-      longitude:
-        detailPlace?.geometry?.location?.lng ?? TUGU_JOGJA_COORDINATE.longitude,
+      latitude: detailPlace?.geometry?.location?.lat,
+      longitude: detailPlace?.geometry?.location?.lng,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     });
@@ -26,12 +19,8 @@ const MapViewRN = () => {
     <MapView ref={ref} provider={PROVIDER_GOOGLE} style={{ flex: 1 }}>
       <Marker
         coordinate={{
-          latitude:
-            detailPlace?.geometry?.location?.lat ??
-            TUGU_JOGJA_COORDINATE.latitude,
-          longitude:
-            detailPlace?.geometry?.location?.lng ??
-            TUGU_JOGJA_COORDINATE.longitude,
+          latitude: detailPlace?.geometry?.location?.lat ?? 0,
+          longitude: detailPlace?.geometry?.location?.lng ?? 0,
         }}
         title={detailPlace.name ?? ''}
         description={detailPlace.formatted_address ?? ''}
